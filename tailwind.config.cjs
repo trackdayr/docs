ead7757a77b4dfd9d94049021d4fe6a7a5bc0201
Luca Pavan <hello@lucapavan.dev>
2022-10-09 19:04:50 +0200

    Added sitemap and anchor icon for content headings

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./astro.config.ts',
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+	],
 	darkMode: 'class',
 	theme: {
 		extend: {
