e4e7ea94ca5f4ce32d1ab9608712e585eca83bd7
Luca Pavan <hello@lucapavan.dev>
2022-10-13 12:57:17 +0200

    Working test with autocomplete

--- src/scripts/markdown-indexing.js
+++ src/scripts/markdown-indexing.js
@@ -1,36 +1,40 @@
-import dotenv from 'dotenv';
-import fs from 'fs';
-import matter from 'gray-matter';
-import algoliasearch from 'algoliasearch';
-import {indexingExcludedPages} from '../config.js';
+import dotenv from "dotenv";
+import fs from "fs";
+import matter from "gray-matter";
+import algoliasearch from "algoliasearch";
+import { indexingExcludedPages } from "../config.js";
 
 dotenv.config();
 
-const pages = fs.readdirSync('src/pages').filter(fn => fn.endsWith('.md') && !indexingExcludedPages.includes(fn));
+const pages = fs
+  .readdirSync("src/pages")
+  .filter((fn) => fn.endsWith(".md") && !indexingExcludedPages.includes(fn));
 console.log(pages);
 
-const objects = pages.map(page => {
-    const fileContents = fs.readFileSync(`src/pages/${page}`, 'utf8');
-    const {data, content} = matter(fileContents);
-    delete data.layout;
-    const path = page.replace('.md', '');
-    let slug = path === 'docs/index' ? 'docs' : path;
-    slug = "/" + slug + "/";
-    return {
-        slug,
-        content,
-        frontmatter: {
-            ...data
-        }
-    };
-})
+const objects = pages.map((page) => {
+  const fileContents = fs.readFileSync(`src/pages/${page}`, "utf8");
+  const { data, content } = matter(fileContents);
+  const path = page.replace(".md", "");
+  let slug = path === "docs/index" ? "docs" : path;
+  slug = "/" + slug + "/";
+  return {
+    title: data.title,
+    description: data.description,
+    slug,
+    content,
+  };
+});
 console.log(objects);
 
 const client = algoliasearch(
-    process.env.ALGOLIA_APP_ID,
-    process.env.ALGOLIA_ADMIN_KEY,
+  process.env.PUBLIC_ALGOLIA_APP_ID,
+  process.env.ALGOLIA_ADMIN_KEY
 );
-const index = client.initIndex('markdown-pages');
-index.saveObjects(objects, {
-    autoGenerateObjectIDIfNotExist: true
+const index = client.initIndex("markdown-pages");
+index.setSettings({
+  searchableAttributes: ["title", "description", "content"],
+  attributesToHighlight: ["content"],
+});
+index.replaceAllObjects(objects, {
+  autoGenerateObjectIDIfNotExist: true,
 });
