684d542d4da894ba1d0dfb0a4e613b8e1ca89f6c
Luca Pavan <hello@lucapavan.dev>
2022-11-09 20:06:19 +0100

    Modified config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,50 +1,64 @@
 export const sidebar = {
-    'Skin modding': [
-        {
-            title: 'Rider skins',
-            filename: 'rider-skins',
-        },
-        {
-            title: 'Bike skins',
-            filename: 'bike-skins',
-        },
-    ],
-    'Asset modding': [
-        {
-            title: 'Bike modding',
-            filename: 'bike-modding',
-        },
-        {
-            title: 'Helmet modding',
-            filename: 'helmet-modding',
-        },
-    ],
-    'Modding tools': [
-        {
-            title: 'Bike tools',
-            filename: 'bike-tools',
-        },
-        {
-            title: 'Track tools',
-            filename: 'track-tools',
-        },
-    ],
-    'Debugging': [
-        {
-            title: 'Advanced Mode',
-            filename: 'advanced-mode',
-        },
-        {
-            title: 'Log Location',
-            filename: 'log-location',
-        },
-    ],
-    'API Reference': [
-        {
-            title: 'GetLapsOfUser',
-            filename: 'get-laps-of-user',
-        },
-    ]
-}
+  "Skin modding": [
+    {
+      title: "Rider skins",
+      filename: "rider-skins",
+    },
+    {
+      title: "Bike skins",
+      filename: "bike-skins",
+    },
+  ],
+  "Asset modding": [
+    {
+      title: "Bike modding",
+      filename: "bike-modding",
+    },
+    {
+      title: "Helmet modding",
+      filename: "helmet-modding",
+    },
+  ],
+  "Modding tools": [
+    {
+      title: "Bike tools",
+      filename: "bike-tools",
+    },
+    {
+      title: "Track tools",
+      filename: "track-tools",
+    },
+  ],
+  Debugging: [
+    {
+      title: "Advanced Mode",
+      filename: "advanced-mode",
+    },
+    {
+      title: "Log Location",
+      filename: "log-location",
+    },
+  ],
+  "API Reference": [
+    {
+      title: "GetNearestFriendsLaps",
+      filename: "get-nearest-friends-laps",
+    },
+    {
+      title: "GetLeaderboardInfo",
+      filename: "get-leaderboard-info",
+    },
+    {
+      title: "NotifyLapSet",
+      filename: "notify-lap-set",
+    },
+  ],
+  Miscellaneous: [
+    {
+      title: "Brand Usage",
+      filename: "brand",
+    },
+  ],
+};
 
-export const defaultPage = 'rider-skins';
+export const defaultPage = "rider-skins";
